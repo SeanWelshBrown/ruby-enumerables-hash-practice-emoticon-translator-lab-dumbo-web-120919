81c8c08b0996d09d54161c222deddf2ef4c91e00
Sean Welsh Brown <seanwelshbrown@gmail.com>
2019-12-06 15:13:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(file_path, emoticon)
       return value
     end
   end
-  puts "Sorry, that emoticon was not found"    
+  return "Sorry, that emoticon was not found"    
 end
 
 def get_english_meaning
